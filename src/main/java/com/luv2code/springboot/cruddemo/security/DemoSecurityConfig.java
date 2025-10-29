83f627f30a600138e9f6bbcdccbf399d17f9da35
Zola Salman <mhmdsalman055@gmail.com>
2025-10-29 13:39:28 +0300

    git add .

--- src/main/java/com/luv2code/springboot/cruddemo/security/DemoSecurityConfig.java
+++ src/main/java/com/luv2code/springboot/cruddemo/security/DemoSecurityConfig.java
@@ -1,39 +1,24 @@
 package com.luv2code.springboot.cruddemo.security;
 
+import javax.sql.DataSource;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class DemoSecurityConfig {
 
     @Bean
-    public InMemoryUserDetailsManager userDetailsManager() {
-
-        UserDetails john = User.builder()
-                .username("john")
-                .password("{noop}test123")
-                .roles("EMPLOYEE")
-                .build();
-
-        UserDetails mary = User.builder()
-                .username("mary")
-                .password("{noop}test123")
-                .roles("EMPLOYEE","MANAGER")
-                .build();
-
-        UserDetails suzan = User.builder()
-                .username("suzan")
-                .password("{noop}test123")
-                .roles("EMPLOYEE", "MANAGER", "ADMIN")
-                .build();
-
-        return new InMemoryUserDetailsManager(john, mary, suzan);
+    public UserDetailsManager userDetailsManager(DataSource dataSource){ 
+JdbcUserDetailsManager theUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+      theUserDetailsManager.setUsersByUsernameQuery(" SELECT user_id,pw, active FROM employee_directory.members where user_id = ? ");
+theUserDetailsManager.setAuthoritiesByUsernameQuery("SELECT user_id,role FROM employee_directory.roles where user_id = ? ");
+        return theUserDetailsManager ;
     }
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws  Exception{
@@ -42,6 +27,7 @@ public class DemoSecurityConfig {
                         .requestMatchers(HttpMethod.GET,"/api/employees/**").hasRole("EMPLOYEE")
                         .requestMatchers(HttpMethod.POST,"/api/employees").hasRole("MANAGER")
                         .requestMatchers(HttpMethod.PUT,"/api/employees/**").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.PATCH,"/api/employees/**").hasRole("MANAGER")
                         .requestMatchers(HttpMethod.DELETE,"/api/employees/**").hasRole("ADMIN")
                         .anyRequest().authenticated()
         );
